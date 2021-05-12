56a109d23055e4e1f080f3bc82c99e110903e37c
choieunsong <greatgelly96@gmail.com>
2021-05-12 17:58:35 +0900

    사전

--- BOJ/BOJ_G3_1256_사전/BOJ_G3_1256_사전.java
+++ BOJ/BOJ_G3_1256_사전/BOJ_G3_1256_사전.java
@@ -3,16 +3,17 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_G3_1256_사전 {
 	// 200C100일 때 자연수 범위 넘어감 
 	static long dp[][];
 	static int N, M, K;
-	static String ans = "";
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader in = new BufferedReader(new StringReader(input));
 		StringTokenizer st = new StringTokenizer(in.readLine()); 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -20,40 +21,39 @@ public class BOJ_G3_1256_사전 {
 		
 		dp = new long[N+1][M+1];
 		
+		for(int n = 0; n <=N; n++) {
+			Arrays.fill(dp[n], 1);
+		}
 		for(int n = 1; n <= N; n++) {
-			dp[n][0] = 1;
 			for(int m = 1; m <= M; m++) {
-				dp[n][m] = Math.min(dp[n-1][m] + dp[n][m-1], 1000000001);
+				dp[n][m] = Math.min(dp[n-1][m] + dp[n][m-1], 1000000001); 
 			}
 		}
-		System.out.println();
-		if(K>dp[N][M])
-			ans = "-1";
-		else
-			find(N,M,K-1);
-		System.out.println(ans);
+		if(K > dp[N][M])	sb.append("-1");	
+		else				getString(N, M);
+		System.out.println(sb.toString());
+	}
+	static void getString(int n, int m) {
+		if(n == 0) {
+			while(--m >= 0) {
+				sb.append("z");
+			}
+			return;
+		}
+		if(m == 0) {
+			while(--n >= 0) {
+				sb.append("a");
+			}
+			return;
+		}
+		if(dp[n-1][m] >= K) {
+			sb.append("a");
+			getString(n-1, m);
+		}else {
+			sb.append("z");
+			K -= dp[n-1][m];
+			getString(n, m-1);
+		}
 	}
 	
-	static void find(int acnt, int zcnt, long left) {
-		if(acnt == 0) {
-			while(zcnt-- > 0) {
-				ans += "z";
-			}
-			return;
-		}
-		if(zcnt == 0) {
-			while(acnt-- > 0) {
-				ans += "a";
-			}
-			return;
-		}
-		long temp = dp[acnt-1][zcnt];
-		if(left < temp) {
-			ans += "a";
-			find(acnt-1, zcnt, left);
-		}else {
-			ans += "z";
-			find(acnt, zcnt-1, left-temp);
-		}
-	}
 }
